22a873fe0461c56b6013fa0e3e49ee7aa374600f
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2023-04-03 09:59:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ port = process.env.port
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(cors());
-app.use('/' , authRoute)
-app.use('/' , authMiddleware,  contacts)
+app.use('/api' , authRoute)
+app.use('/api' , authMiddleware,  contacts)
 
 
-app.get('/' , authMiddleware, (req,res)=>{
+app.get('/' , (req,res)=>{
     res.send('hello world')
 })
 
